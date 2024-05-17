b2a6deb6a8515751dcbeca3c7c032eb56139c58d
KaraKiz <uemuehanay@gmail.com>
2024-05-17 13:30:43 +0200

    Profileinstellungen angepasst

--- app/controllers/profile_controller.ts
+++ app/controllers/profile_controller.ts
@@ -4,42 +4,57 @@ import hash from '@adonisjs/core/services/hash';
 import app from '@adonisjs/core/services/app';
 import { cuid } from '@adonisjs/core/helpers';
 
-
 export default class ProfileController {
 
     public async updateProfilePicture({ request, session, response, view }: HttpContext) {
 
-        const { image, telephone, email, firstname, lastname, username, password, newPassword } = request.all();
+        const { telephone, email, firstname, lastname, username, password, newPassword } = request.all();
         const user = session.get('user');
 
-        let finalPassword = null;
-        if (newPassword && newPassword.trim() !== '') {
-            finalPassword = await hash.make(newPassword);
-        } else {
-            finalPassword = user.password;
-        }
+        let finalPassword = user.password;
 
-        const img = request.file('image');
-        if (!img) {
-
+        if (password && newPassword) {
             if (password === newPassword) {
-                console.log("Passwords has to be different!");
+                console.log("Passwords have to be different!");
                 return view.render('pages/profile', { user, profileError: "Passwörter dürfen nicht gleich sein!" });
             }
-    
+
             const uppercaseChars = newPassword.match(/[A-Z]/g);
             const digitChars = newPassword.match(/[0-9]/g);
             if (newPassword.length < 8 || !uppercaseChars || uppercaseChars.length < 2 || !digitChars || digitChars.length < 2) {
                 console.log("Password has to contain 8 letters");
-                return view.render('pages/profile', { user,profileError: 'Das Passwort muss mindestens 8 Zeichen lang sein und mindestens 2 Großbuchstaben sowie 2 Zahlen enthalten!' });
+                return view.render('pages/profile', { user, profileError: 'Das Passwort muss mindestens 8 Zeichen lang sein und mindestens 2 Großbuchstaben sowie 2 Zahlen enthalten!' });
             }
 
-            if(finalPassword != session.get('user').password){
+            const isOldPasswordCorrect = await hash.verify(user.password, password);
+            if (!isOldPasswordCorrect) {
                 console.log("Old Password not correct");
-                return view.render('pages/profile', { user,profileError: 'Altes Passwort stimmt nicht!' });
+                return view.render('pages/profile', { user, profileError: 'Altes Passwort stimmt nicht!' });
             }
 
-            
+            finalPassword = await hash.make(newPassword);
+        }
+
+        const img = request.file('image');
+        if (img) {
+            const fileName = `${cuid()}.${img.extname}`;
+            await img.move(app.publicPath('uploads'), { name: fileName });
+            const fullFilePath = `${app.publicPath('uploads')}/${fileName}`;
+
+            await db.from('users').where('id', user.id).update({
+                firstname: firstname || user.firstname,
+                lastname: lastname || user.lastname,
+                email: email || user.email,
+                username: username || user.username,
+                password: finalPassword,
+                telephone: telephone || user.telephone,
+                profile_picture: fileName,
+                path: fullFilePath
+            });
+
+            user.profile_picture = fileName;
+            user.path = fullFilePath;
+        } else {
             await db.from('users').where('id', user.id).update({
                 firstname: firstname || user.firstname,
                 lastname: lastname || user.lastname,
@@ -48,70 +63,17 @@ export default class ProfileController {
                 password: finalPassword,
                 telephone: telephone || user.telephone
             });
-
-            user.firstname = firstname || user.firstname;
-            user.lastname = lastname || user.lastname;
-            user.email = email || user.email;
-            user.username = username || user.username;
-            user.telephone = telephone || user.telephone;
-
-            session.put('user', user);
-
-
-            const userAds = await db.from('newad').where('user_id', user.id);
-
-            return response.redirect().toRoute('/profile', { user, userAds });
         }
 
-      
-        if (password === newPassword) {
-            console.log("Passwords has to be different!");
-            return view.render('pages/profile', { user, profileError: "Passwörter dürfen nicht gleich sein!" });
-        }
-
-        const uppercaseChars = password.match(/[A-Z]/g);
-        const digitChars = password.match(/[0-9]/g);
-        if (password.length < 8 || !uppercaseChars || uppercaseChars.length < 2 || !digitChars || digitChars.length < 2) {
-            console.log("Password criteria not met");
-            return view.render('pages/profile', { user,profileError: 'Das Passwort muss mindestens 8 Zeichen lang sein und mindestens 2 Großbuchstaben sowie 2 Zahlen enthalten!' });
-        }
-
-
-        
-        const fileName = `${cuid()}.${img.extname}`;
-
-        await img.move(app.publicPath('uploads'), { name: fileName });
-
-        const fullFilePath = `${app.publicPath('uploads')}/${fileName}`;
-
-        //Anpassung der Profildaten
-        await db.from('users').where('id', user.id).update({
-            firstname: firstname || user.firstname,
-            lastname: lastname || user.lastname,
-            email: email || user.email,
-            username: username || user.username,
-            password: finalPassword,
-            telephone: telephone || user.telephone,
-            profile_picture: fileName || user.profile_picture,
-            path: fullFilePath || user.path
-        });
-
-        await db.from('newad').where('user_id', user.id).select('*').update({
-            user_id: user.id
-        });
-
-        //falls die restlichen Profildaten gelcih bleibene,w erden deise nicht verändert
         user.firstname = firstname || user.firstname;
         user.lastname = lastname || user.lastname;
         user.email = email || user.email;
         user.username = username || user.username;
         user.telephone = telephone || user.telephone;
-        user.profile_picture = fileName || user.profile_picture;
-        user.path = fullFilePath || user.path;
 
         session.put('user', user);
 
-        console.log(image, email, firstname, lastname, username, password, newPassword);
+        console.log(email, firstname, lastname, username, password, newPassword);
 
         const userAds = await db.from('newad').where('user_id', user.id);
 
@@ -123,9 +85,7 @@ export default class ProfileController {
         if (user) {
             const newads = await db.from('newad').where('user_id', user.id);
             const messages = await db.from('messages').where('sender_id', user.id);
-            return view.render('pages/home', {user, newads, messages });
+            return view.render('pages/home', { user, newads, messages });
         }
     }
-
-
 }
